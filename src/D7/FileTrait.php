f6d290c4eb70955b6fa9d7f9a4635dadcf0d7818
Alex Skrypnyk <alex.designworks@gmail.com>
2018-07-05 23:36:52 +1000

    Fixed file trait.

--- src/D7/FileTrait.php
+++ src/D7/FileTrait.php
@@ -12,8 +12,6 @@ use Behat\Gherkin\Node\TableNode;
 trait FileTrait {
 
   /**
-   * Create managed file from existing local file.
-   *
    * @Given managed file:
    */
   public function fileCreateManaged(TableNode $nodesTable) {
@@ -23,25 +21,34 @@ trait FileTrait {
       if (empty($node->path)) {
         throw new \RuntimeException('"path" property is required');
       }
+      $path = ltrim($node->path, '/');
 
       // Limited support for remote files: all remote files are considered
       // oembed objects and therefore only oembed'able objects will be saved.
       if (parse_url($node->path, PHP_URL_SCHEME) !== NULL) {
-        $provider = media_internet_get_provider($node->path);
+        $provider = media_internet_get_provider($path);
         $file = $provider->save();
       }
       // Local file.
       else {
-        $file_path = dirname(dirname(__FILE__)) . '/' . ltrim($node->path, '/');
-        $destination = 'public://' . basename($file_path);
-        if (!is_readable($file_path)) {
-          throw new \RuntimeException('Unable to find file ' . $file_path);
+        // Get fixture file path.
+        if ($this->getMinkParameter('files_path')) {
+          $full_path = rtrim(realpath($this->getMinkParameter('files_path')), DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR . $path;
+          if (is_file($full_path)) {
+            $path = $full_path;
+          }
         }
-        $file = file_save_data(file_get_contents($file_path), $destination, FILE_EXISTS_REPLACE);
+
+        if (!is_readable($path)) {
+          throw new \RuntimeException('Unable to find file ' . $path);
+        }
+
+        $destination = 'public://' . basename($path);
+        $file = file_save_data(file_get_contents($path), $destination, FILE_EXISTS_REPLACE);
       }
 
       if (!$file) {
-        throw new \RuntimeException('Unable to save managed file ' . $file_path);
+        throw new \RuntimeException('Unable to save managed file ' . $path);
       }
     }
   }
