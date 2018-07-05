5410fc79c5cd2f620b95e7449d8f189742a30d9e
Alex Skrypnyk <alex.designworks@gmail.com>
2018-07-05 21:19:55 +1000

    Added more paths traits.

--- src/PathTrait.php
+++ src/PathTrait.php
@@ -2,6 +2,8 @@
 
 namespace IntegratedExperts\BehatSteps;
 
+use Behat\Mink\Exception\ExpectationException;
+
 /**
  * Trait PathTrait.
  *
@@ -19,8 +21,31 @@ trait PathTrait {
     $current_path = $current_path == '' ? '<front>' : $current_path;
 
     if ($current_path != ltrim($path, '/')) {
-      throw new \Exception(sprintf('Current path is "%s", but expected is "%s"', $current_path, $path));
+      throw new ExpectationException(sprintf('Current path is "%s", but expected is "%s"', $current_path, $path));
     }
+  }
+
+  /**
+   * @Then I should not be in the :path path
+   */
+  public function pathAssertNotCurrent($path) {
+    $current_path = $this->getSession()->getCurrentUrl();
+    $current_path = parse_url($current_path, PHP_URL_PATH);
+    $current_path = ltrim($current_path, '/');
+
+    if ($current_path == $path) {
+      throw new ExpectationException(sprintf('Current path should not be "%s"', $current_path));
+    }
+
+    return TRUE;
+  }
+
+  /**
+   * @When I visit :path then the final URL should be :alias
+   */
+  public function assertAtPathRedirect($path, $alias) {
+    $this->getSession()->visit($this->locatePath($path));
+    $this->pathAssertCurrent($alias);
   }
 
 }
