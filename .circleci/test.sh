403e9fd32605f3f3ed8dd21de2d3dc7d5e01a0dd
Alex Skrypnyk <alex@integratedexperts.com>
2020-11-13 17:54:28 +1100

    Updated stack and D8 test fixtures.

--- .circleci/test.sh
+++ .circleci/test.sh
@@ -7,8 +7,5 @@ set -e
 echo "==> Lint code"
 ahoy lint
 
-echo "==> Run tests"
-ahoy test-bdd
-
 echo "==> Run BDD tests"
 ahoy test-bdd -- "--format=progress_fail" || ahoy test-bdd -- "--rerun --format=progress_fail"
