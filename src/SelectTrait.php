201ded38d9c4696fcb61d0c3b0de5bb43da599d0
Richard Gaunt <richard.gaunt@vu.edu.au>
2021-02-22 17:46:28 +1100

    Refactored SelectTrait.

--- src/SelectTrait.php
+++ src/SelectTrait.php
@@ -22,6 +22,8 @@ trait SelectTrait {
     if (is_null($optionElement)) {
       throw new \InvalidArgumentException(sprintf('Option "%s" is not found in select "%s".', $option, $select));
     }
+
+    return $optionElement;
   }
 
   /**
@@ -43,14 +45,7 @@ trait SelectTrait {
    * @Then select :select should have option :option selected
    */
   public function selectShouldHaveOptionSelected($select, $option) {
-    $selectElement = $this->getSession()->getPage()->findField($select);
-    if (is_null($selectElement)) {
-      throw new \InvalidArgumentException(sprintf('Element "%s" is not found.', $select));
-    }
-    $optionElement = $selectElement->find('named', ['option', $option]);
-    if (is_null($optionElement)) {
-      throw new \InvalidArgumentException(sprintf('Option "%s" is not found in select "%s".', $option, $select));
-    }
+    $optionElement = $this->selectShouldHaveOption($select, $option);
     if ($optionElement->getAttribute('selected') !== 'selected') {
       throw new \InvalidArgumentException(sprintf('Option "%s" in select "%s" is not selected.', $option, $select));
     }
@@ -60,14 +55,7 @@ trait SelectTrait {
    * @Then select :select should not have option :option selected
    */
   public function selectShouldHaveNotOptionSelected($select, $option) {
-    $selectElement = $this->getSession()->getPage()->findField($select);
-    if (is_null($selectElement)) {
-      throw new \InvalidArgumentException(sprintf('Element "%s" is not found.', $select));
-    }
-    $optionElement = $selectElement->find('named', ['option', $option]);
-    if (is_null($optionElement)) {
-      throw new \InvalidArgumentException(sprintf('Option "%s" is not found in select "%s".', $option, $select));
-    }
+    $optionElement = $this->selectShouldHaveOption($select, $option);
     if ($optionElement->getAttribute('selected') === 'selected') {
       throw new \InvalidArgumentException(sprintf('Option "%s" in select "%s" is selected, but should not.', $option, $select));
     }
