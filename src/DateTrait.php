1e19208a546fe5c243749d5d2ebc575f0c7b46bb
Alex Skrypnyk <alex@integratedexperts.com>
2022-08-15 19:41:57 +1000

    Added relative date processing.

--- src/DateTrait.php
+++ src/DateTrait.php
@@ -2,6 +2,8 @@
 
 namespace DrevOps\BehatSteps;
 
+use Behat\Gherkin\Node\TableNode;
+
 /**
  * Trait DateTrait.
  *
@@ -10,6 +12,47 @@ namespace DrevOps\BehatSteps;
  * @package DrevOps\BehatSteps
  */
 trait DateTrait {
+
+  /**
+   * Transform a scalar value.
+   *
+   * @Transform :value
+   */
+  public function dateRelativeTransformValue($value) {
+    return static::dateRelativeProcessValue($value);
+  }
+
+  /**
+   * Transform a tabular value.
+   *
+   * @Transform table:*
+   */
+  public function dateRelativeTransformTable(TableNode $table) {
+    // Inexpensive token detection and early exit.
+    if (!static::dateRelativeStringHasToken($table->getTableAsString())) {
+      return $table;
+    }
+
+    $rows = [];
+    foreach ($table->getRows() as $hash) {
+      $row = [];
+      foreach ($hash as $column => $cell) {
+        $row[$column] = self::dateRelativeProcessValue($cell);
+      }
+      $rows[] = $row;
+    }
+
+    $new_table = new TableNode($rows);
+
+    return $new_table;
+  }
+
+  /**
+   * Check if sting has a token.
+   */
+  protected static function dateRelativeStringHasToken($string) {
+    return strpos($string, '[relative:') !== FALSE;
+  }
 
   /**
    * Process date values to convert relative timestamps to actual values.
@@ -30,17 +73,22 @@ trait DateTrait {
    * | test article | [relative:-1 day] |
    * @endcode
    *
-   * @note: Since return value can be a date string, it is possible that
+   * @note Since return value can be a date string, it is possible that
    * asserted result may span across multiple days (i.e. if set as -14 hours).
    * To avoid this, default time is always rounded to midday and it is expected
    * that relative time within a day use max of 12 hours offset.
    */
-  public static function dateProcess($value, $now = NULL) {
+  public static function dateRelativeProcessValue($value, $now = NULL) {
+    // Inexpensive token detection and early exit.
+    if (!static::dateRelativeStringHasToken($value)) {
+      return $value;
+    }
+
     // If `now` is not provided, round to the current hour to make sure that
     // assertions are running within the same timeframe (for long tests).
-    $now = $now ? $now : strtotime(date('Y-m-d H:i:00', time()));
+    $now = $now ?: strtotime(date('Y-m-d H:i:00', time()));
 
-    return preg_replace_callback('/\[([^:]+):([^\]\[\#]+)(?:\#([^\]\[]+))?\]/', function ($matches) use ($now) {
+    return preg_replace_callback('/\[([relative:]+):([^]\[#]+)(?:#([^]\[]+))?]/', function ($matches) use ($now) {
       $timestamp = strtotime($matches[2], $now);
       if ($timestamp === FALSE) {
         throw new \RuntimeException(sprintf('The supplied relative date cannot be evaluated: "%s"', $matches[1]));
---
+++ tests/behat/features/date.feature
@@ -0,0 +1,22 @@
+@d9
+Feature: Check that DateTrait works
+
+  @api
+  Scenario: Assert that table transform works
+    Given I am logged in as a user with the "administrator" role
+    When I go to "node/add/article"
+    And I fill in "Title" with "[TEST] Article 1 first [relative:-10 years#Y-m-d] and second [relative:-9 years#Y-m-d]"
+    And I select "Published" from "edit-moderation-state-0-state"
+    And I press "Save"
+    Then the response status code should be 200
+    And I should see the text "[TEST] Article 1 first 201"
+    And I should see the text "and second 201"
+
+  @api
+  Scenario: Assert that table transform works
+    Given "article" content:
+      | title            | created              | status | moderation_state |
+      | [TEST] Article 1 | [relative:-10 years] | 1      | published        |
+    When I visit article "[TEST] Article 1"
+    Then the response status code should be 200
+    And I should see the text "201"
