4dc5ee76a1b05663f46f35e84d6bd2ef985c0e85
richardgaunt <57734756+richardgaunt@users.noreply.github.com>
2023-12-18 18:44:46 +1100

    Added handling for multiple sessions in bigpipe. (#148)

--- src/BigPipeTrait.php
+++ src/BigPipeTrait.php
@@ -3,6 +3,8 @@
 namespace DrevOps\BehatSteps;
 
 use Behat\Behat\Hook\Scope\BeforeScenarioScope;
+use Behat\Behat\Hook\Scope\BeforeStepScope;
+use Behat\Mink\Exception\DriverException;
 use Behat\Mink\Exception\UnsupportedDriverActionException;
 use Drupal\big_pipe\Render\Placeholder\BigPipeStrategy;
 
@@ -14,11 +16,31 @@ use Drupal\big_pipe\Render\Placeholder\BigPipeStrategy;
 trait BigPipeTrait {
 
   /**
+   * Flag for JS not supported by driver.
+   *
+   * @var bool
+   */
+  protected $bigPipeNoJS;
+
+  /**
+   * Skip Big Pipe BeforeStep.
+   *
+   * @var bool
+   */
+  protected bool $bigPipeBeforeStepSkip = FALSE;
+
+  /**
    * Prepares Big Pipe NOJS cookie if needed.
    *
    * @BeforeScenario
    */
   public function bigPipeBeforeScenarioInit(BeforeScenarioScope $scope) {
+    // Allow to skip resetting cookies on step.
+    // BeforeStep scope does not have access to scenario where tagging is
+    // made.
+    if ($scope->getScenario()->hasTag('behat-steps-skip:bigPipeBeforeStep')) {
+      $this->bigPipeBeforeStepSkip = TRUE;
+    }
     // Allow to skip this by adding a tag.
     if ($scope->getScenario()->hasTag('behat-steps-skip:' . __FUNCTION__)) {
       return;
@@ -36,14 +58,37 @@ trait BigPipeTrait {
         $driver->start();
       }
       $driver->executeScript('true');
+      $this->bigPipeNoJS = FALSE;
     }
     catch (UnsupportedDriverActionException $e) {
+      $this->bigPipeNoJS = TRUE;
       $this
         ->getSession()
         ->setCookie(BigPipeStrategy::NOJS_COOKIE, 'true');
     }
     catch (\Exception $e) {
       // Mute exceptions.
+    }
+  }
+
+  /**
+   * Prepares Big Pipe NO JS cookie if needed.
+   *
+   * @BeforeStep
+   */
+  public function bigPipeBeforeStep(BeforeStepScope $scope) {
+    if ($this->bigPipeBeforeStepSkip) {
+      return;
+    }
+    try {
+      if ($this->bigPipeNoJS && !$this->getSession()->getCookie(BigPipeStrategy::NOJS_COOKIE)) {
+        $this->getSession()
+          ->setCookie(BigPipeStrategy::NOJS_COOKIE, 'true');
+      }
+    }
+    catch (DriverException $e) {
+      // Mute not visited page exception.
+      return;
     }
   }
 
--- tests/behat/features/big_pipe.feature
+++ tests/behat/features/big_pipe.feature
@@ -12,3 +12,28 @@ Feature: Check that BigPipeTrait works for or D9
     Given I install a "big_pipe" module
     When I visit "/"
     Then cookie "big_pipe_nojs" does not exist
+
+  @api
+  Scenario: Assert that Big Pipe cookie is preserved across multiple users in a scenario
+    Given users:
+      | name                        | mail                                      | roles         | status |
+      | administrator_user          | administrator_user@myexample.com          | administrator | 1      |
+    And I install a "big_pipe" module
+    When I visit "/"
+    Then cookie "big_pipe_nojs" exists
+    And I am logged in as "administrator_user"
+    And I visit "/"
+    Then cookie "big_pipe_nojs" exists
+
+  @api @behat-steps-skip:bigPipeBeforeStep
+  Scenario: Assert that Big Pipe cookie is not preserved across multiple users when skip tag is used
+    Given users:
+      | name                        | mail                                      | roles         | status |
+      | administrator_user          | administrator_user@myexample.com          | administrator | 1      |
+    And I install a "big_pipe" module
+    When I visit "/"
+    Then cookie "big_pipe_nojs" exists
+    # Logging in as a new user removes cookies.
+    And I am logged in as "administrator_user"
+    When I visit "/"
+    Then cookie "big_pipe_nojs" does not exist
