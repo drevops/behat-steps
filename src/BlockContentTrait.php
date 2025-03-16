265fc768402f3409676348ef5842d665b1cff956
Richard Gaunt <richard.gaunt@protonmail.com>
2025-03-16 20:26:42 +1100

    Add BlockContentTrait to test block_content and BlockTrait to test blocks.

---
+++ src/BlockContentTrait.php
@@ -0,0 +1,211 @@
+<?php
+
+declare(strict_types=1);
+
+use Behat\Behat\Hook\Scope\AfterScenarioScope;
+use Drupal\block_content\Entity\BlockContent;
+use Behat\Gherkin\Node\TableNode;
+use Drupal\block_content\BlockContentTypeInterface;
+
+/**
+ * Provides Behat step definitions for managing block_content entities.
+ */
+trait BlockContentTrait {
+
+  /**
+   * Array of created block_content entities.
+   *
+   * @var array<int,\Drupal\block_content\Entity\BlockContent>
+   */
+  protected static $blockContentEntities = [];
+
+  /**
+   * Clean all block_content instances after scenario run.
+   *
+   * @AfterScenario
+   */
+  public function blockContentCleanAll(AfterScenarioScope $scope): void {
+    // Allow to skip this by adding a tag.
+    if ($scope->getScenario()->hasTag('behat-steps-skip:' . __FUNCTION__)) {
+      return;
+    }
+
+    foreach (static::$blockContentEntities as $block_content) {
+      $block_content->delete();
+    }
+    static::$blockContentEntities = [];
+  }
+
+  /**
+   * Assert that a vocabulary exist.
+   *
+   * @code
+   * Given block_content_type "civictheme_search" with description "Search" exists
+   * @endcode
+   *
+   * @Given block_content_type :type with desciprion :description exists
+   */
+  public function contentBlockAssertTypeExist(string $description, string $type): void {
+    $block_content_type = \Drupal::entityTypeManager()->getStorage('block_content_type')->load($type);
+
+    if (!$block_content_type instanceof BlockContentTypeInterface) {
+      throw new \Exception(sprintf('"%s" block_content_type does not exist', $type));
+    }
+
+    if ($block_content_type->get('info') !== $description) {
+      throw new \Exception(sprintf('"%s" block_content_type name is not "%s"', $type, $description));
+    }
+  }
+
+  /**
+   * Assert that a block_content exist by name.
+   *
+   * @code
+   * Given block_content "Search" of block_content_type "civictheme_search" exists
+   * @endcode
+   *
+   * @Given block_content :name of block_content_type :type exists
+   */
+  public function contentBlockAssertTermExistsByName(string $name, string $type): void {
+    $block_content_type = \Drupal::entityTypeManager()->getStorage('block_content_type')->load($type);
+
+    if ($block_content_type instanceof BlockContentTypeInterface) {
+      throw new \Exception(sprintf('"%s" block_content_type does not exist', $type));
+    }
+
+    $found = \Drupal::entityTypeManager()
+      ->getStorage('block_content')
+      ->loadByProperties([
+        'info' => $name,
+        'type' => $block_content_type,
+      ]);
+
+    if (count($found) == 0) {
+      throw new \Exception(sprintf('Block content "%s" of type "%s" does not exist', $name, $type));
+    }
+  }
+
+  /**
+   * Remove content blocks from a specified content_block_type.
+   *
+   * @code
+   * Given no "Fruits" content blocks:
+   * | Apple |
+   * | Pear  |
+   * @endcode
+   *
+   * @Given no :type block_content:
+   */
+  public function contentBlockDeleteTerms(string $type, TableNode $contentBlockTable): void {
+    foreach ($contentBlockTable->getColumn(0) as $description) {
+      $content_blocks = \Drupal::entityTypeManager()->getStorage('block_content')->loadByProperties([
+        'description' => $description,
+        'type' => $type,
+      ]);
+
+      foreach ($content_blocks as $content_block) {
+        /** @var \Drupal\block_content\Entity\BlockContent $content_block */
+        $content_block->delete();
+      }
+    }
+  }
+
+  /**
+   * Visit specified block_content_type block_content edit page.
+   *
+   * @When I edit :type block_content_type with description :info
+   */
+  public function blockContentEditBlockContentPageWithName(string $type, string $info): void {
+    $block_ids = $this->contentBlockLoadMultiple($type, [
+      'info' => $info,
+    ]);
+
+    if (empty($block_ids)) {
+      throw new \RuntimeException(sprintf('Unable to find %s term "%s"', $type, $info));
+    }
+
+    ksort($block_ids);
+    $block_id = end($block_ids);
+
+    $path = $this->locatePath('/admin/content/block/' . $block_id . '/edit');
+    print $path;
+
+    $this->getSession()->visit($path);
+  }
+
+  /**
+   * Load multiple content blocks with specified type and conditions.
+   *
+   * @param string $type
+   *   The block content type.
+   * @param array<string,string> $conditions
+   *   Conditions keyed by field names.
+   *
+   * @return array<int, string>
+   *   Array of block content ids.
+   */
+  protected function contentBlockLoadMultiple(string $type, array $conditions = []): array {
+    $query = \Drupal::entityQuery('block_content')
+      ->accessCheck(FALSE)
+      ->condition('type', $type);
+
+    foreach ($conditions as $k => $v) {
+      $and = $query->andConditionGroup();
+      $and->condition($k, $v);
+      $query->condition($and);
+    }
+
+    return $query->execute();
+  }
+
+  /**
+   * Creates block_content of a given type with field data.
+   *
+   * @param string $type
+   *   Content block_content type.
+   * @param \Behat\Gherkin\Node\TableNode $block_content_table
+   *   Field data for a block_content.
+   *
+   * @Given :type block_content:
+   *
+   * @code
+   *   Given "help" block_content:
+   *  | title    | status | created           |
+   *  | My title | 1      | 2014-10-17 8:00am |
+   * @endcode
+   */
+  public function createBlockContents(string $type, TableNode $block_content_table): void {
+    foreach ($block_content_table->getHash() as $blockContentHash) {
+      $this->createBlockContent($type, $blockContentHash);
+    }
+  }
+
+  /**
+   * Create a block content entity and place if region set.
+   *
+   * @param string $type
+   *   Block content type.
+   * @param array<string> $values
+   *   Values for block content entity.
+   *
+   * @return \Drupal\block_content\Entity\BlockContent
+   *   Created block content entity.
+   *
+   * @throws \Drupal\Core\Entity\EntityStorageException
+   *
+   * @SuppressWarnings(PHPMD.StaticAccess)
+   */
+  protected function createBlockContent(string $type, array $values): BlockContent {
+    $block_content = (object) $values;
+    $block_content->type = $type;
+    $this->parseEntityFields('block_content', $block_content);
+    $block_content = (array) $block_content;
+    /** @var \Drupal\block_content\Entity\BlockContent $block_content */
+    $block_content = BlockContent::create($block_content);
+    $block_content->save();
+    static::$blockContentEntities[] = $block_content;
+
+    return $block_content;
+  }
+
+}
---
+++ src/BlockTrait.php
@@ -0,0 +1,356 @@
+<?php
+
+declare(strict_types=1);
+
+use Behat\Behat\Hook\Scope\AfterScenarioScope;
+use Behat\Gherkin\Node\TableNode;
+use Drupal\block\Entity\Block;
+
+/**
+ * Provides Behat step definitions for managing blocks programmatically.
+ */
+trait BlockTrait {
+
+  /**
+   * Array of created block instances.
+   *
+   * @var array<int, \Drupal\block\Entity\Block>
+   */
+  protected static array $blockInstances = [];
+
+  /**
+   * Create, configure and place a block in the default theme region.
+   *
+   * @code
+   * @When I create a block of type :label with:
+   * | label         | [TEST] Welcome Message      |
+   * | display_label | 1                           |
+   * | region        | <region>                    |
+   * | status        | 1                           |
+   * @endcode
+   *
+   * @SuppressWarnings(PHPMD.StaticAccess)
+   */
+  public function placeBlockInRegion(string $label, TableNode $fields): void {
+    /** @var \Drupal\Core\Block\BlockManagerInterface $block_manager */
+    $block_manager = \Drupal::service('plugin.manager.block');
+    $definitions = $block_manager->getDefinitions();
+    $default_theme = \Drupal::config('system.theme')->get('default');
+    $block = NULL;
+    foreach ($definitions as $plugin_id => $definition) {
+      if ((string) $definition['admin_label'] === $label) {
+        $block = \Drupal::entityTypeManager()->getStorage('block')->create([
+          'plugin' => $plugin_id,
+          'theme' => $default_theme,
+        ]);
+        $suggestion = $block->getPlugin()->getMachineNameSuggestion();
+        $block_id = \Drupal::service('block.repository')->getUniqueMachineName($suggestion, $block->getTheme());
+        $block->set('id', $block_id);
+        break;
+      }
+    }
+    if (!$block instanceof Block) {
+      throw new \Exception(sprintf('Could not create block of type "%s"', $label));
+    }
+    $this->configureBlockInstance($block, $fields);
+  }
+
+  /**
+   * Clean all created blocks after scenario run.
+   *
+   * @AfterScenario
+   */
+  public function blockInstanceCleanAll(AfterScenarioScope $scope): void {
+    // Allow to skip this by adding a tag.
+    if ($scope->getScenario()->hasTag('behat-steps-skip:' . __FUNCTION__)) {
+      return;
+    }
+
+    foreach (static::$blockInstances as $block) {
+      $block->delete();
+    }
+    static::$blockInstances = [];
+  }
+
+  /**
+   * Find a block in default theme with a specified label and configure.
+   *
+   * @code
+   * @When I configure the block with the label :label with:
+   *  | label         | [TEST] Welcome Message      |
+   *  | display_label | 1                           |
+   *  | region        | <region>                    |
+   *  | status        | 1                           |
+   * @endcode
+   */
+  public function configureBlock(string $description, TableNode $fields): void {
+    $block = $this->loadBlockByLabel($description);
+
+    $this->configureBlockInstance($block, $fields);
+
+    $block->save();
+  }
+
+  /**
+   * Configure block instance.
+   *
+   * @param \Drupal\block\Entity\Block $block
+   *   Block to be configured.
+   * @param \Behat\Gherkin\Node\TableNode $fields
+   *   Provide fields to configure.
+   */
+  protected function configureBlockInstance(Block $block, TableNode $fields): void {
+    foreach ($fields->getRowsHash() as $field => $value) {
+      switch ($field) {
+        case 'label':
+          $settings = $block->get('settings');
+          $settings['label'] = $value;
+          $block->set('settings', $settings);
+          break;
+
+        case 'label_display':
+          $settings = $block->get('settings');
+          $settings['label_display'] = (bool) $value;
+          $block->set('settings', $settings);
+          break;
+
+        case 'region':
+          $block->setRegion($value);
+          break;
+
+        case 'status':
+          $block->setStatus((bool) $value);
+          break;
+      }
+    }
+    $block->save();
+  }
+
+  /**
+   * Sets a visibility condition for a block.
+   *
+   * @param string $label
+   *   Label identifying the block.
+   * @param string $condition
+   *   The type of visibility condition.
+   * @param \Behat\Gherkin\Node\TableNode $fields
+   *   Field data table.
+   *
+   * @When I configure the visibility condition :condition for the block with label :label
+   */
+  public function configureBlockVisibility(string $label, string $condition, TableNode $fields): void {
+    $block = $this->loadBlockByLabel($label);
+    $configuration = $fields->getRowsHash();
+    $configuration['id'] = $condition;
+    $block->setVisibilityConfig($condition, $configuration);
+    $block->save();
+  }
+
+  /**
+   * Removes a visibility condition from the block.
+   *
+   * @param string $label
+   *   Label identifying the block.
+   * @param string $condition
+   *   The type of visibility condition.
+   *
+   * @When I remove the visibility condition :condition from the block with label :label
+   */
+  public function removeBlockVisibility(string $label, string $condition): void {
+    $this->configureBlockVisibility($label, $condition, new TableNode([]));
+  }
+
+  /**
+   * Disables a block specified with a label.
+   *
+   * @param string $label
+   *   Label used to identify a block.
+   *
+   * @When I disable the block with label :label
+   *
+   * @throws \Drupal\Core\Entity\EntityStorageException
+   */
+  public function disableBlock(string $label): void {
+    $block = $this->loadBlockByLabel($label);
+    $block->disable();
+    $block->save();
+  }
+
+  /**
+   * Enables a block specified with a label.
+   *
+   * @param string $label
+   *   Label used to identify a block.
+   *
+   * @When I enable the block with label :label
+   *
+   * @throws \Drupal\Core\Entity\EntityStorageException
+   */
+  public function enableBlock(string $label): void {
+    $block = $this->loadBlockByLabel($label);
+    $block->enable();
+    $block->save();
+  }
+
+  /**
+   * Asserts that a block exists.
+   *
+   * @param string $label
+   *   Label used to identify a block.
+   *
+   * @Then block with label :label should exist
+   */
+  public function assertBlockExists(string $label): void {
+    $default_theme = \Drupal::config('system.theme')->get('default');
+    $blocks = \Drupal::entityTypeManager()
+      ->getStorage('block')
+      ->loadByProperties([
+        'settings.label' => $label,
+        'theme' => $default_theme,
+      ]);
+
+    if (empty($blocks)) {
+      throw new \Exception(sprintf('Block with title "%s" was not found', $label));
+    }
+  }
+
+  /**
+   * Asserts that a block exists in a specified region.
+   *
+   * @param string $label
+   *   Label used to identify a block.
+   * @param string $region
+   *   Region to check for the block in.
+   *
+   * @When block with label :label should exist in the region :region
+   */
+  public function assertBlockExistsInRegion(string $label, string $region): void {
+    $blocks = \Drupal::entityTypeManager()
+      ->getStorage('block')
+      ->loadByProperties([
+        'settings.label' => $label,
+        'region' => $region,
+      ]);
+
+    if (empty($blocks)) {
+      throw new \Exception(sprintf('Block with title "%s" was not found in region "%s"', $label, $region));
+    }
+  }
+
+  /**
+   * Asserts that a block does not exist in a specified region.
+   *
+   * @param string $label
+   *   Label used to identify a block.
+   * @param string $region
+   *   Region to check for the block in.
+   *
+   * @When block with label :label should not exist in the region :region
+   */
+  public function assertBlockDoesNotExistInRegion(string $label, string $region): void {
+    $blocks = \Drupal::entityTypeManager()
+      ->getStorage('block')
+      ->loadByProperties([
+        'settings.label' => $label,
+        'region' => $region,
+      ]);
+
+    if (!empty($blocks)) {
+      throw new \Exception(sprintf('Block with title "%s" was found in region "%s but should not have been."', $label, $region));
+    }
+  }
+
+  /**
+   * Asserts that the block has a visibility condition.
+   *
+   * @param string $label
+   *   Label identifying the block.
+   * @param string $condition
+   *   The type of visibility condition.
+   *
+   * @Then the block with label :label should have the visibility condition :condition
+   */
+  public function assertBlockHasCondition(string $label, string $condition): void {
+    $block = $this->loadBlockByLabel($label);
+    $conditions = $block->getVisibilityConditions();
+
+    if (!$conditions->has($condition)) {
+      throw new \Exception(sprintf('Block "%s" does not have condition "%s"', $label, $condition));
+    }
+  }
+
+  /**
+   * Assert that the block should not have a visibility condition.
+   *
+   * @param string $label
+   *   Label identifying the block.
+   * @param string $condition
+   *   The type of visibility condition.
+   *
+   * @Then the block with label :label should not have the visibility condition :condition
+   */
+  public function assertBlockShouldNotHaveCondition(string $label, string $condition): void {
+    $block = $this->loadBlockByLabel($label);
+    $conditions = $block->getVisibilityConditions();
+
+    if ($conditions->has($condition)) {
+      throw new \Exception(sprintf('Block "%s" should not have condition "%s"', $label, $condition));
+    }
+  }
+
+  /**
+   * Assert that the block with specified label is disabled.
+   *
+   * @param string $label
+   *   Label to identify block with.
+   *
+   * @Then the block with label :label is disabled
+   */
+  public function assertBlockIsDisabled(string $label): void {
+    $block = $this->loadBlockByLabel($label);
+    if ($block->status()) {
+      throw new \Exception(sprintf('Block "%s" is not disabled and should be.', $label));
+    }
+  }
+
+  /**
+   * Assert that the block with specified label is enabled.
+   *
+   * @param string $label
+   *   Label to identify block with.
+   *
+   * @Then the block with label :label is enabled
+   */
+  public function assertBlockIsNotDisabled(string $label): void {
+    $block = $this->loadBlockByLabel($label);
+    if (!$block->status()) {
+      throw new \Exception(sprintf('Block "%s" is disabled but should not be.', $label));
+    }
+  }
+
+  /**
+   * Loads a block by its description/admin label.
+   *
+   * @param string $label
+   *   Label to find the block by.
+   *
+   * @return \Drupal\block\Entity\Block
+   *   Loaded block.
+   */
+  private function loadBlockByLabel(string $label): Block {
+    $default_theme = \Drupal::config('system.theme')->get('default');
+    $blocks = \Drupal::entityTypeManager()
+      ->getStorage('block')
+      ->loadByProperties([
+        'theme' => $default_theme,
+        'settings.label' => $label,
+      ]);
+
+    if (empty($blocks)) {
+      throw new \Exception(sprintf('Block with label "%s" was not found', $label));
+    }
+
+    return reset($blocks);
+  }
+
+}
